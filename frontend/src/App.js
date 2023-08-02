dae15286adb72fb950147e9a3308a68cc7d3a09f
arriann <arriann.lee@gmail.com>
2023-08-02 23:53:56 +0100

    Added browser router added links for homescreen and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">Zoomies & Co</a>
-      </header>
-
-      <main>list products here</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Zoomies & Co</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
