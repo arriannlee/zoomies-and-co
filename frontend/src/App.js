9b5f10e79108ba980a30c8dffd90a1ea31d5f540
arriann <arriann.lee@gmail.com>
2023-08-02 22:17:15 +0100

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Zoomies & Co</a>
+      </header>
+
+      <main>list products here</main>
+    </div>
+  );
+}
+
+export default App;
