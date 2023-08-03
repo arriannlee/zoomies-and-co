1049e0611933ccc8e8902db373b67f0852870dd1
arriann <arriann.lee@gmail.com>
2023-08-03 14:46:17 +0100

    Updated product

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,19 @@
+function Product(props) {
+  const { product } = props;
+  return (
+    <Card key={product.slug}>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <div className="product-info">
+        <Link to={`/product/${product.slug}`}>
+          <p>{product.name}</p>
+        </Link>
+        <p>£{product.price}</p>
+        <button>Add to Basket </button>
+      </div>
+    </Card>
+  );
+}
+
+export default Product;
